54d4fea48ebdc957db2dc36bdcf13c340f0e0505
Masahiro Yamada <yamada.masahiro@socionext.com>
2016-09-13 13:36:51 +0900

    pinctrl: uniphier: move internal macros
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

--- drivers/pinctrl/uniphier/pinctrl-uniphier-core.c
+++ drivers/pinctrl/uniphier/pinctrl-uniphier-core.c
@@ -26,6 +26,14 @@
 #include "../pinctrl-utils.h"
 #include "pinctrl-uniphier.h"
 
+#define UNIPHIER_PINCTRL_PINMUX_BASE	0x1000
+#define UNIPHIER_PINCTRL_LOAD_PINMUX	0x1700
+#define UNIPHIER_PINCTRL_DRVCTRL_BASE	0x1800
+#define UNIPHIER_PINCTRL_DRV2CTRL_BASE	0x1900
+#define UNIPHIER_PINCTRL_DRV3CTRL_BASE	0x1980
+#define UNIPHIER_PINCTRL_PUPDCTRL_BASE	0x1a00
+#define UNIPHIER_PINCTRL_IECTRL		0x1d00
+
 struct uniphier_pinctrl_priv {
 	struct pinctrl_desc pctldesc;
 	struct pinctrl_dev *pctldev;
