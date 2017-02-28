77d05b277d1927613a78b46ccfe7ec662a119dc8
Masahiro Yamada <yamada.masahiro@socionext.com>
2017-02-28 22:46:30 +0900

    mtd: nand: denali: handle timing parameters by setup_data_interface()
    The timing parameter handling in this driver is bad in multiple ways.
    
    [1] The ONFi Timing Mode is specified by the module parameter
        "onfi_timing_mode".  Users are free to set any Timing Mode that
        may not be supported on the device.
    
    [2] The function nand_onfi_timing_set() holds many parameters in it.
        It is duplicating efforts of drivers/mtd/nand/nand_timings.c
    
    [3] The function get_onfi_nand_para() detects the supported Timing
        Modes.  The driver need/should not have its own way because the
        ONFi parameters are read by nand_flash_detect_onfi() and the
        supported Timing Modes are detected by nand_init_data_interface().
    
    [4] Intel specific parameters such as CLK_X=5, CLK_MULTI=4, etc. are
        hard-coded in the driver, so this function is not working for
        other platforms.
    
    [5] It is weird for a driver to take care of a particular device
        (Samsung K9WAG08U1A) by get_samsung_nand_para().
    
    Now, the core framework provides a common logic to handle timings.
    Implement .setup_data_interface().
    
    While I am working on this, I found even more issues in the current
    code, so fixed the following as well:
    
    - In recent IP versions, WE_2_RE and TWHR2 share the same register.
      Likewise for ADDR_2_DATA and TCWAW, CS_SETUP_CNT and TWB.  When
      updating one, the other must be masked.  Otherwise, the other will
      be set to 0, then timing settings will be broken.
    
    - The recent IP release expanded the ADDR_2_DATA to 7-bit wide.
      This register is related to tADL.  As commit 74a332e78e8f ("mtd:
      nand: timings: Fix tADL_min for ONFI 4.0 chips") addressed, the
      ONFi 4.0 increased the minimum of tADL to 400 nsec.  This may not
      fit in the 6-bit ADDR_2_DATA in older versions.  Check the IP
      revision and handle this correctly, otherwise the register value
      would wrap around.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Series-changes: 2
      - Newly added

--- drivers/mtd/nand/denali_dt.c
+++ drivers/mtd/nand/denali_dt.c
@@ -93,7 +93,6 @@ static int denali_dt_probe(struct platform_device *pdev)
 		denali->caps = data->caps;
 	}
 
-	denali->platform = DT;
 	denali->dev = &pdev->dev;
 	denali->irq = platform_get_irq(pdev, 0);
 	if (denali->irq < 0) {
@@ -119,6 +118,8 @@ static int denali_dt_probe(struct platform_device *pdev)
 		return PTR_ERR(dt->clk);
 	}
 	clk_prepare_enable(dt->clk);
+
+	denali->clk_x_rate = clk_get_rate(dt->clk);
 
 	ret = denali_init(denali);
 	if (ret)
