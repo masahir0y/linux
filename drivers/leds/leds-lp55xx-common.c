972ab7415dd9dd7820425d727a97c2f1a9dbc89b
Masahiro Yamada <yamada.masahiro@socionext.com>
2017-05-16 00:33:41 +0900

    led
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

--- drivers/leds/leds-lp55xx-common.c
+++ drivers/leds/leds-lp55xx-common.c
@@ -447,8 +447,7 @@ void lp55xx_deinit_device(struct lp55xx_chip *chip)
 {
 	struct lp55xx_platform_data *pdata = chip->pdata;
 
-	if (chip->clk)
-		clk_disable_unprepare(chip->clk);
+	clk_disable_unprepare(chip->clk);
 
 	if (gpio_is_valid(pdata->enable_gpio))
 		gpio_set_value(pdata->enable_gpio, 0);
