f4473e9e28461dc6072b798d9a7b9dc6840ac9f7
Masahiro Yamada <yamada.masahiro@socionext.com>
2019-05-16 17:40:52 +0900

    kbuild: check MODULE_* macros in non-modular code
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

--- scripts/modules-check.sh
+++ scripts/modules-check.sh
@@ -13,4 +13,58 @@ check_same_name_modules()
 	done
 }
 
+# Check MODULE_ macros in non-modular code
+check_bogus_module_macros()
+{
+	if [ ! -r modules.builtin.modinfo ]; then
+		return
+	fi
+
+	# modules.builtin lists *real* built-in modules. In other words,
+	# controlled by tristate CONFIG options, but currently built with =y.
+	#
+	# On the other hand, modules.builtin.modinfo is the list of MODULE_
+	# macros compiled into vmlinux, including ones from
+	# never-compiled-as-module code.
+	#
+	# By diff'ing them, users of bogus MODULE_* macros will show up.
+
+	# Kbuild replaces ',' and '-' in file names with '_' for use in C
+	real_builtin_modules=$(cat modules.builtin | \
+				xargs basename -s .ko -- 2>/dev/null | \
+				tr ',-' '__' | sort | uniq)
+
+	show_fix=
+
+	# Exclude '.paramtype=' because module_param() is legitimately used by
+	# non-modular code. Exclude '.param='; do not check MODULE_PARAM_DESC()
+	# because it might be useful.
+	module_macro_users=$(tr '\0' '\n' < modules.builtin.modinfo | \
+		sed -e '/\.parmtype=/d' -e '/\.parm=/d' | \
+		sed -n 's/\..*//p' | sort | uniq)
+	for m in $module_macro_users
+	do
+		warn=1
+
+		for n in $real_builtin_modules
+		do
+			if [ "$m" = "$n" ]; then
+				warn=
+				break
+			fi
+		done
+
+		if [ -n "$warn" ]; then
+			echo "notice: $m: MODULE macros found in non-modular code"
+			show_fix=1
+		fi
+	done
+
+	if [ -n "$show_fix" ]; then
+		echo "  To fix above, check bogus MODULE_LICENSE(), MODULE_AUTHOR(), etc."
+		echo "  Pleae check #include <linux/module.h>, THIS_MODULE, too."
+	fi
+}
+
 check_same_name_modules
+check_bogus_module_macros
